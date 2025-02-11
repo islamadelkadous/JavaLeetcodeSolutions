1f1221ed56370aecab28d5198aab8ad325305376
Islam Adel Kadous <48600816+islamadelkadous@users.noreply.github.com>
2025-02-11 16:28:55 +0300

    Create 1910.java

---
+++ 1910.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String removeOccurrences(String s, String part) {
+        while (s.contains(part)) {
+            s = s.replaceFirst(part, "");
+        }
+        return s;
+    }
+}
+/*
+class Solution {
+  public String removeOccurrences(String s, String part) {
+    final int n = s.length();
+    final int k = part.length();
+
+    StringBuilder sb = new StringBuilder(s);
+    int j = 0; // sb's index
+
+    for (int i = 0; i < n; ++i) {
+      sb.setCharAt(j++, s.charAt(i));
+      if (j >= k && sb.substring(j - k, j).toString().equals(part))
+        j -= k;
+    }
+
+    return sb.substring(0, j).toString();
+  }
+}
+*/
