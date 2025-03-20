11ef32eac3fdf57f8d6e338afeda13ecb6a6a027
Islam Adel Kadous <48600816+islamadelkadous@users.noreply.github.com>
2025-03-20 20:51:59 +0300

    Update 1492.java

--- 1492.java
+++ 1492.java
@@ -1,6 +1,6 @@
 class Solution {
     public int kthFactor(int n, int k) {
-        for (int i = 1; i<= n ; i++) {
+        for (int i = 1; i<= n/2 ; i++) {
             if (n % i == 0) {
                 k--;
             }
@@ -8,6 +8,6 @@ class Solution {
                 return i;
             }
         }
-        return -1;
+        return k==1 ? n : -1;
     }
 }
