4281da72bc26906ac802e060c6e24e5a1982dbd7
Islam Adel Kadous <48600816+islamadelkadous@users.noreply.github.com>
2025-07-26 21:25:50 +0300

    Update 1768.java

--- 1768.java
+++ 1768.java
@@ -13,3 +13,27 @@ class Solution {
         return sb.toString();
     }
 }
+/*
+another solution
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int ptr1 = 0, ptr2 = 0;
+        String result = "";
+        while(ptr1 < word1.length() || ptr2 < word2.length()) {
+            if (ptr1 >= word1.length()) {
+                result += word2.charAt(ptr2);
+                ptr2++;
+            } else if (ptr2 >= word2.length()) {
+                result += word1.charAt(ptr1);
+                ptr1++;
+            } else {
+                result += word1.charAt(ptr1);
+                ptr1++;
+                result += word2.charAt(ptr2);
+                ptr2++;
+            }
+        }
+        return result;
+    }
+}
+*/
